aa22d6c4e9843d699b35842add9886165a05887b
FStriewski <fstriewski@outlook.com>
2018-05-21 16:52:11 +0200

    Move Filter components

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -11,11 +11,6 @@ import Input, { InputLabel } from 'material-ui/Input';
 
 
 const styles = theme => ({
-    root: {
-        width: '100%',
-        marginTop: theme.spacing.unit * 1,
-        overflowX: 'auto',
-    },
     container: {
          display: 'flex',
     },
@@ -23,47 +18,13 @@ const styles = theme => ({
         marginLeft: 20,
         width: 200,
     },
-    formControl: {
-        width: 200,
-        marginBottom: 10,
-    }
 });
 
 
-class FilterBar extends PureComponent {
-    static propTypes = {
-        companys: PropTypes.arrayOf(PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            name: PropTypes.string.isRequired,
-            market: PropTypes.string,
-            focus: PropTypes.string,
-            score: PropTypes.number.isRequired,
-            language: PropTypes.oneOf(['INT', 'NL']),
-            applied: PropTypes.oneOf(['y', 'n']),
-            link: PropTypes.string.isRequired,
-            comments: PropTypes.string,
-        })).isRequired,
-
-        filterByTerm: PropTypes.func.isRequired,
-        undo: PropTypes.func.isRequired,
-    }
-
-    handleChange = (e) => {
-        let filter = this.props.companys.present.filter(company => company.name.toLowerCase().includes        (e.target.value.toLowerCase()) )
-      
-        return this.props.filterByTerm(filter)
-    }
-
-
-    handleBackspace = (e) => {
-        if (e.keyCode === 8 && this.props.companys.past.length > 0) {
-           return this.props.undo()
-         }
-         return 
-    }
-
+ class FilterBar extends PureComponent {
+   
     render(){
-        const { companys, classes } = this.props
+        const { classes } = this.props
         return (
             <div className={classes.container}>
                 <Term />
@@ -76,12 +37,6 @@ class FilterBar extends PureComponent {
     }
 }
 
-const mapStateToProps = (state) => ({
-        companys: state.companys
-    })
-
-
 export default combine(
     withStyles(styles),
-    connect(mapStateToProps, { filterByTerm, undo})
 )(FilterBar)
